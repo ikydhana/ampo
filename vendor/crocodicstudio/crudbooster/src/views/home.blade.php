120f60a0974292e97a77ae7fa3f8aa9afaa601c4
ikydhana <rwardana998@gmail.com>
2020-02-11 09:25:46 +0800

    v 1.3.0

--- vendor/crocodicstudio/crudbooster/src/views/home.blade.php
+++ vendor/crocodicstudio/crudbooster/src/views/home.blade.php
@@ -1,6 +1,41 @@
 @extends('crudbooster::admin_template')
 
 @section('content')
-
-
+<img style="  display:block;margin-left: auto;margin-right:auto;width:18%;" src="http://smkn1banjarbaru.sch.id/wp-content/uploads/2018/07/logo-smik-besar-245x300.png">
+<br>
+<br>
+<h1><p style="text-align:center;"><b>AMPO (Aplikasi Monitoring Prakerin Online)</b></p></h1>
+<br>
+<br>
+<h1><p style="text-align:center;">VISI</p></h1>
+<h4><p style="text-align:justify;">Terwujudnya lulusan yang bermoral, kompeten, mandiri, unggul dan berwawasan lingkungan.</p></h4>
+<br>
+<br>
+<h1><p style="text-align:center;">Misi</p></h1>
+<h4><p style="text-align:justify;">- Melaksanakan pembinaan mental keagamaan, kerja dan sosial.</p></h4>
+<br>
+<h4><p style="text-align:justify;">- Menumbuhkan sikap mandiri, kreatif, inovatif, kompetitif dan tidak mudah putus asa.</p></h4>
+<br>
+<h4><p style="text-align:justify;">- Meningkatkan mutu lulusan yang unggul dan kompeten untuk memenuhi tuntutan dunia kerja</p></h4>
+<br>
+<h4><p style="text-align:justify;">- Meningkatkan kepedulian terhadap lingkungan yang rindang, bersih, rapi dan sehat.</p></h4>
+<br>
+<h4><p style="text-align:justify;">- Mewujudkan lingkungan yang nyaman aman, rindang, asri dan bersih</p></h4>
+<br>
+<h4><p style="text-align:justify;">- Mencegah terjadinya pencemaran lingkungan</p></h4>
+<br>
+<h4><p style="text-align:justify;">- Mencegah terjadinya kerusakan lingkungan</p></h4>
+<br>
+<h4><p style="text-align:justify;">- Mewujudkan perilaku 3R (Re-duce, Re-use, dan Re-cycle)</p></h4>
+<br>
+<br>
+<h1><p style="text-align:center;">Tujuan</p></h1>
+<br>
+<h4><p style="text-align:justify;">- Menghasilkan lulusan yang berakhlak mulia, cerdas dan terampil.</p></h4>
+<br>
+<h4><p style="text-align:justify;">- Menghasilkan lulusan yang memiliki ilmu pengetahuan dan teknologi untuk melanjutkan ke jenjang pendidikan yang lebih tinggi.</p></h4>
+<br>
+<h4><p style="text-align:justify;">- Mewujudkan lulusan yang mandiri sesuai dengan tuntutan dunia usaha/kerja.</p></h4>
+<br>
+<h4><p style="text-align:justify;">- Mewujudkan lulusan yang peduli terhadap lingkungan.</p></h4>
 @endsection
