aa2ab783b78800db034d6af8d73bdf6fa481d240
ikydhana <rwardana998@gmail.com>
2020-03-11 10:00:49 +0800

    UPDATE lagi

---
+++ database/seeds/KegiatanTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class KegiatanTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        \DB::table('kegiatan')->delete();
+        
+        
+        
+    }
+}
